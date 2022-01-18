e77fe3e3347ff2685b07984fbca3ad5dc10a578e
Aakash Goyal <aakashgoyal@GK-Pro13-Dec2021-5115s-MacBook-Pro.local>
2022-01-18 00:29:47 -0800

    Addon

---
+++ app/components/restaurantDetail/AddOn.js
@@ -0,0 +1,129 @@
+import React from 'react';
+import {
+  View,
+  StyleSheet,
+  Button,
+  Modal,
+  Image,
+  Text,
+  TouchableOpacity,
+  Animated,
+} from 'react-native';
+import AddOnItem from './AddOnItem';
+
+const foods = [
+  {
+    id : 1,
+    title: 'Extra',
+    description: 'Cheese',
+    price: '$0',
+    image:
+      'https://www.modernhoney.com/wp-content/uploads/2019/08/Classic-Lasagna-14-scaled.jpg',
+  
+    },
+  {
+    id : 2,
+    title: 'Sauce',
+    description: '🔥',
+    price: '$0',
+    image: 'https://i.ytimg.com/vi/BKxGodX9NGg/maxresdefault.jpg',
+  },
+  {
+    id : 3,
+    title: 'Random',
+    description:'Santa',
+    price: '$0',
+    image:
+      'https://i2.wp.com/chilipeppermadness.com/wp-content/uploads/2020/11/Chilaquales-Recipe-Chilaquiles-Rojos-1.jpg',
+  },
+  
+];
+
+const ModalPoup = ({visible, children}) => {
+  const [showModal, setShowModal] = React.useState(visible);
+  const scaleValue = React.useRef(new Animated.Value(0)).current;
+  React.useEffect(() => {
+    toggleModal();
+  }, [visible]);
+  const toggleModal = () => {
+    if (visible) {
+      setShowModal(true);
+      Animated.spring(scaleValue, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      setTimeout(() => setShowModal(false), 200);
+      Animated.timing(scaleValue, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    }
+  };
+  return (
+    <Modal transparent visible={showModal}>
+      <View style={styles.modalBackGround}>
+        <Animated.View
+          style={[styles.modalContainer, {transform: [{scale: scaleValue}]}]}>
+          {children}
+        </Animated.View>
+      </View>
+    </Modal>
+  );
+};
+
+const AddOn = () => {
+  const [visible, setVisible] = React.useState(false);
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <ModalPoup visible={visible}>
+        <View style={{alignItems: 'center'}}>
+          <View style={styles.header}>
+            <TouchableOpacity onPress={() => setVisible(false)}>
+              <Image
+               source={require('../../assets/images/x.png')}
+                style={{height: 30, width: 30}}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={{alignItems: 'center'}}>
+        <AddOnItem  foods={foods} />
+          
+        </View>
+
+        <Text style={{marginVertical: 30, fontSize: 20, textAlign: 'center'}}>
+          AddOns
+        </Text>
+      </ModalPoup>
+      <Button title="Add Ons" onPress={() => setVisible(true)} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalBackGround: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContainer: {
+    width: '95%',
+    backgroundColor: 'white',
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    borderRadius: 20,
+    elevation: 20,
+  },
+  header: {
+    width: '100%',
+    height: 40,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+  },
+});
+
+export default AddOn;
